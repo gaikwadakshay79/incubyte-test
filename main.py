167cfa3c5a85b442400a30c63c40563ca5e1b51b
= <=>
2024-05-24 16:51:51 +0530

    [REFACTOR] Code changes done for adding extract delimiter and number string method

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 ## All code will go here
 
-def add(string_numbers):
-    if string_numbers == "":
-        return 0
-    
+def extract_delimiter_and_number_string(string_numbers):
     delimiter = ","
 
     if string_numbers[:2] == "//":
         delimiter = string_numbers[2]
         string_numbers = string_numbers[3:]
+    return delimiter, string_numbers
+
+
+def add(string_numbers):
+    if string_numbers == "":
+        return 0
+    
+    delimiter, string_numbers = extract_delimiter_and_number_string(string_numbers)
 
     numbers_list = string_numbers.replace("\n", delimiter).split(delimiter)
     numbers_sum = sum(map(int, numbers_list))
