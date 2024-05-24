7886ceb2ee2b24c49f8dd39e6d7695a3a36b5545
= <=>
2024-05-24 16:28:14 +0530

    Added test cases for empty string, single variable, two variables

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 ## All code will go here
+
+def add(string_numbers):
+    return 0
--- tests.py
+++ tests.py
@@ -1,1 +1,16 @@
 ## All tests will go here
+import unittest
+from main import add
+
+class TestStringMethods(unittest.TestCase):
+    def test_checking_empty_string(self):
+        self.assertEqual(add(""), 0)
+
+    def test_checking_single_variable_input(self):
+        self.assertEqual(add("1"), 1)
+
+    def test_checking_multiple(self):
+        self.assertEqual(add("1,5"), 6)
+
+if __name__ == '__main__':
+    unittest.main()
