7e0ad8bac980bfe9c6cee2967eec5348aeecaa51
= <=>
2024-05-24 16:48:39 +0530

    Code changes done for allowing dynamic delimiter input

--- main.py
+++ main.py
@@ -3,8 +3,14 @@
 def add(string_numbers):
     if string_numbers == "":
         return 0
+    
+    delimiter = ","
 
-    numbers_list = string_numbers.replace("\n", ",").split(",")
+    if string_numbers[:2] == "//":
+        delimiter = string_numbers[2]
+        string_numbers = string_numbers[3:]
+
+    numbers_list = string_numbers.replace("\n", delimiter).split(delimiter)
     numbers_sum = sum(map(int, numbers_list))
 
     return numbers_sum
