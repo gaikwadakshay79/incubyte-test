05bacf08edb13c8dc2adc08ba77a36e206475c4d
= <=>
2024-05-24 16:55:00 +0530

    [REFACTOR] Code changes done for get numbers list method

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ def extract_delimiter_and_number_string(string_numbers):
     if string_numbers[:2] == "//":
         delimiter = string_numbers[2]
         string_numbers = string_numbers[3:]
-    return delimiter, string_numbers
+    return string_numbers, delimiter
 
+def get_numbers_list(numbers_string, delimiter):
+    numbers_string_list = numbers_string.replace("\n", delimiter).split(delimiter)
+    numbers_list = list(map(int, numbers_string_list))
+    return numbers_list
 
 def add(string_numbers):
     if string_numbers == "":
         return 0
     
-    delimiter, string_numbers = extract_delimiter_and_number_string(string_numbers)
+    string_numbers, delimiter = extract_delimiter_and_number_string(string_numbers)
+    numbers_list = get_numbers_list(string_numbers, delimiter)
 
-    numbers_list = string_numbers.replace("\n", delimiter).split(delimiter)
-    numbers_sum = sum(map(int, numbers_list))
-
+    numbers_sum = sum(numbers_list)
     return numbers_sum
