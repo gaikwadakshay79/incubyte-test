511376f615d5e6ec91a2ce93437102260f86f965
= <=>
2024-05-24 16:40:50 +0530

    Code changes done for allowing new line character as separator

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def add(string_numbers):
     if string_numbers == "":
         return 0
 
-    numbers_list = string_numbers.split(",")
+    numbers_list = string_numbers.replace("\n", ",").split(",")
     numbers_sum = sum(map(int, numbers_list))
 
     return numbers_sum
